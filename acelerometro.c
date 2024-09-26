208fae4285ee1384021f5b55d062a19202fdb43c
brendatrindade <brendatrindde@gmail.com>
2024-09-26 16:08:10 -0300

    :sparkles: feat: acesso ao acelerometro
    Realizar comunicacao com o acelerometro ADXL345

---
+++ acelerometro.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define HPS_PHYS_BASE 0xFF000000
+#define HPS_SPAN 0x01000000   
+#define SYSMGR_BASE 0xFFD08000
+#define I2C0_BASE 0xFFC04000 
+
+#define SYSMGR_I2C0USEFPGA (SYSMGR_BASE + 0x704)
+#define SYSMGR_GENERALIO7 (SYSMGR_BASE + 0x49C)
+#define SYSMGR_GENERALIO8 (SYSMGR_BASE + 0x4A0)
+
+#define I2C0_CON (I2C0_BASE + 0x00) 
+#define I2C0_TAR (I2C0_BASE + 0x04)
+#define I2C0_DATA_CMD (I2C0_BASE + 0x10)
+#define I2C0_FS_SCL_HCNT (I2C0_BASE + 0x1C)
+#define I2C0_FS_SCL_LCNT (I2C0_BASE + 0x20)
+#define I2C0_ENABLE (I2C0_BASE + 0x6C) 
+#define I2C0_RXFLR (I2C0_BASE + 0x78)   
+#define I2C0_TXFLR (I2C0_BASE + 0x74)
+#define I2C0_CLR_INTR (I2C0_BASE + 0x40)
+
+#define ADXL345_ADDR 0x53   
+#define ADXL345_DEVID 0x00 
+#define ADXL345_POWER_CTL 0x2D
+#define ADXL345_DATA_FORMAT 0x31
+#define ADXL345_DATAX0 0x32 
+#define ADXL345_DATAX1 0x33 
+#define ADXL345_INT_ENABLE 0x2E
+#define ADXL345_INT_MAP 0x2F
+#define ADXL345_INT_SOURCE 0x30
+#define ADXL345_BW_RATE 0x2C
